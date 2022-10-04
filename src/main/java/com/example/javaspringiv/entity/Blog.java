f52aef706198c7d273274801bbbcc9658978df9d
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-04 18:12:47 -0300

    feat: Repository read values in blogs json file

--- src/main/java/com/example/javaspringiv/entity/Blog.java
+++ src/main/java/com/example/javaspringiv/entity/Blog.java
@@ -1,5 +1,9 @@
 package com.example.javaspringiv.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/example/javaspringiv/repository/BlogRepo.java
+++ src/main/java/com/example/javaspringiv/repository/BlogRepo.java
@@ -1,8 +1,29 @@
 package com.example.javaspringiv.repository;
 
+import com.example.javaspringiv.entity.Blog;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Repository
 public class BlogRepo {
+    private final String linkFile = "src/main/resources/blogs.json";
+    private final ObjectMapper mapper = new ObjectMapper();
+    private List<Blog> blogList = new ArrayList<>();
+
+    public BlogRepo() {
+        try {
+            mapper.findAndRegisterModules();
+            blogList.addAll(Arrays.asList(mapper.readValue(new File(linkFile), Blog[].class)));
+        } catch (Exception ex) {
+            System.out.println("Falha ao ler o arquivo");
+        }
+    }
+
 
 }
