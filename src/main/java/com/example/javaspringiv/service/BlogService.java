83634ee2ef9ea67648c42007503a4e0d19a439f5
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-04 18:32:51 -0300

    feat: BlogService implemented

--- src/main/java/com/example/javaspringiv/service/BlogService.java
+++ src/main/java/com/example/javaspringiv/service/BlogService.java
@@ -1,7 +1,32 @@
 package com.example.javaspringiv.service;
 
+import com.example.javaspringiv.dto.BlogDTO;
+import com.example.javaspringiv.entity.Blog;
+import com.example.javaspringiv.interfaces.IBlogService;
+import com.example.javaspringiv.repository.BlogRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class BlogService {
+public class BlogService implements IBlogService {
+
+    @Autowired
+    private BlogRepo repo;
+
+    @Override
+    public int create(BlogDTO blog) {
+        return repo.create(blog);
+    }
+
+    @Override
+    public List<Blog> getAll() {
+        return null;
+    }
+
+    @Override
+    public Blog get(int id) {
+        return null;
+    }
 }
