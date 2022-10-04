cf3d38fba4275e74cad60791200c398aae4e7135
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-04 18:30:07 -0300

    feat: create method created in BlogRepo

--- src/main/java/com/example/javaspringiv/dto/BlogDTO.java
+++ src/main/java/com/example/javaspringiv/dto/BlogDTO.java
@@ -1,12 +1,17 @@
 package com.example.javaspringiv.dto;
 
 import com.example.javaspringiv.entity.Blog;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
 public class BlogDTO {
     String title, author;
     LocalDateTime publicationDate;
+    int id;
 
     public BlogDTO(Blog blog) {
         this.author = blog.getAuthor();
--- src/main/java/com/example/javaspringiv/repository/BlogRepo.java
+++ src/main/java/com/example/javaspringiv/repository/BlogRepo.java
@@ -1,7 +1,11 @@
 package com.example.javaspringiv.repository;
 
+import com.example.javaspringiv.dto.BlogDTO;
 import com.example.javaspringiv.entity.Blog;
+import com.example.javaspringiv.util.GenerateId;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
@@ -14,6 +18,7 @@ public class BlogRepo {
     private final String linkFile = "src/main/resources/blogs.json";
     private final ObjectMapper mapper = new ObjectMapper();
     private List<Blog> blogList = new ArrayList<>();
+    GenerateId generateId;
 
     public BlogRepo() {
         try {
@@ -22,5 +27,21 @@ public class BlogRepo {
         } catch (Exception ex) {
             System.out.println("Falha ao ler o arquivo");
         }
+        generateId = GenerateId.getInstance();
+    }
+
+    public int create(BlogDTO blog) {
+        int id = generateId.getId();
+        blog.setId(id);
+        blogList.add(new Blog(blog.getId(), blog.getTitle(), blog.getAuthor(), blog.getPublicationDate()));
+
+        try {
+            ObjectWriter writter = mapper.writer(new DefaultPrettyPrinter());
+            writter.writeValue(new File(linkFile), blogList);
+        } catch (Exception ex) {
+            System.out.println("Não foi possível escrever no arquivo");
+        }
+
+        return id;
     }
 }
