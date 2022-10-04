5b39787f918cb6d9d3af7dc4e8342f1d4df696b1
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-04 16:30:48 -0300

    feat: initial structure created

---
+++ src/main/java/com/example/javaspringiv/JavaSpringIvApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringIvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringIvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspringiv/JavaSpringIvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringIvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
