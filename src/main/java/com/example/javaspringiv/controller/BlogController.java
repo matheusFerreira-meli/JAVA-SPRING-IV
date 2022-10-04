61cae248d3ad5582b4e671c56be9858225c220b7
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-04 19:00:24 -0300

    feat: get Blog method implemented in BlogController

--- src/main/java/com/example/javaspringiv/controller/BlogController.java
+++ src/main/java/com/example/javaspringiv/controller/BlogController.java
@@ -2,12 +2,14 @@ package com.example.javaspringiv.controller;
 
 import com.example.javaspringiv.dto.BlogDTO;
 import com.example.javaspringiv.dto.ResponseCreatedBlogDTO;
+import com.example.javaspringiv.entity.Blog;
 import com.example.javaspringiv.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.time.LocalDateTime;
 
 @RestController
@@ -20,5 +22,10 @@ public class BlogController {
     @PostMapping
     public ResponseEntity<ResponseCreatedBlogDTO> create(@RequestBody BlogDTO blog) {
         return new ResponseEntity<>(service.create(blog), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Blog> get(@PathVariable int id) {
+        return new ResponseEntity<>(service.get(id), HttpStatus.OK);
     }
 }
